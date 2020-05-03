0801290f57d55f3aaf0c3eca9d130017ff0628fb
Bruno Souza <brunoprogramadorjava@gmail.com>
2020-05-03 04:06:41 -0300

    [03] Load Balance com ribbon

--- loja/src/main/java/com/brunosouza/loja/service/CompraService.java
+++ loja/src/main/java/com/brunosouza/loja/service/CompraService.java
@@ -1,6 +1,7 @@
 package com.brunosouza.loja.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -8,18 +9,27 @@ import org.springframework.web.client.RestTemplate;
 
 import com.brunosouza.loja.controller.dto.InfoFornecedorDTO;
 import com.brunosouza.loja.controller.dto.compraDTO;
+//import com.netflix.discovery.DiscoveryClient;
 
 @Service
 public class CompraService {
 	
 	@Autowired
 	private RestTemplate client;
+	
+	@Autowired
+	private DiscoveryClient eurekaClient;
 
 	public void realizarCompra(compraDTO compra) {
 		
 		ResponseEntity<InfoFornecedorDTO> exchange = 
 		client.exchange("http://fornecedor/info/"+compra.getEndereco().getEstado(), 
 				HttpMethod.GET, null, InfoFornecedorDTO.class);
+		
+		eurekaClient.getInstances("fornecedor").stream()
+		.forEach(fornecedor -> {
+			System.out.println("localhost" + fornecedor.getPort());
+		});
 		
 		System.out.println(exchange.getBody().getEndereco());
 	}
